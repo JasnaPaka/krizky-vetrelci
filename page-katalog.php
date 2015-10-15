921bce37da166f62ea41a3f03b027a5afdf683ec
Pavel Cvrček <jasnapaka@jasnapaka.com>
2015-10-15 19:46:27 +0200

    Přidání výpisu kategorií děl.

--- page-katalog.php
+++ page-katalog.php
@@ -25,14 +25,16 @@
 <h2>
 	<?php if ($oc->getIsShowedTag()) { ?>
 		Díla se štítkem '<?php printf ($oc->getCurrentTag()->nazev) ?>'
+	<?php } else if ($oc->getIsShowedCategory()) { ?>		
+		Kategorie děl '<?php printf ($oc->getCurrentCategory()->nazev) ?>'
 	<?php } else if ($oc->getSearchValue() == null) { ?>
 		Katalog děl
 	<?php } else { ?>
-		Výsledek hledání v dílech pro "<?php echo $oc->getSearchValue()?>"
+		Výsledek hledání v dílech pro "<?php printf($oc->getSearchValue()) ?>"
 	<?php }?>
 </h2>
 
-<?php if ($page == 0 && count($oc->getAllTags()) > 0) { ?>	
+<?php if ($page == 0 && count($oc->getAllTags()) > 0 && !$oc->getIsShowedCategory()) { ?>	
 	<div id="kat-tags">	
 <?php foreach ($oc->getAllTags() as $tag) { ?>
 
@@ -77,28 +79,29 @@
 			$img = get_template_directory_uri()."-child-krizkyavetrelci/images/foto-neni-340.png";
 		}
 		
-		if ($objCount % 3 == 1) echo '<div>';
+		if ($objCount % 3 == 1) printf('<div>');
 ?>
 
 <div class="post postitem">
-	<a href="/katalog/dilo/<?php echo $objekt->id ?>/" title="Zobrazení informací o díle">
-		<img src="<?php echo $img ?>" alt="Ukázka díla" class="katalog-dilo-obr" />
+	<a href="/katalog/dilo/<?php printf($objekt->id) ?>/" title="Zobrazení informací o díle">
+		<img src="<?php printf($img) ?>" alt="Ukázka díla" class="katalog-dilo-obr" />
 	</a>
 	
 	<div class="padding">
-		<h3><a href="/katalog/dilo/<?php echo $objekt->id ?>/" title="Zobrazení informací o díle"><?php echo $objekt->nazev ?></a></h3>
-		<p><span class="post-label">Kategorie:</span> <?php echo $objekt->katnazev ?></p>
-		<p><span class="post-label"><?php if (count($objekt->autori) > 1) { echo "Autoři"; } else { echo "Autor"; } ?>:</span>
+		<h3><a href="/katalog/dilo/<?php printf($objekt->id) ?>/" title="Zobrazení informací o díle"><?php printf($objekt->nazev) ?></a></h3>
+		<p><span class="post-label">Kategorie:</span> 
+			<a href="/katalog/kategorie/<?php printf($objekt->kategorie) ?>/" title="Zobrazení děl v kategorii"><?php printf($objekt->katnazev) ?></a></p>
+		<p><span class="post-label"><?php if (count($objekt->autori) > 1) { printf("Autoři"); } else { printf("Autor"); } ?>:</span>
 			<?php if (count($objekt->autori) == 0) { ?>
 				nejsou známi
 			<?php } else {
 				$isFirst = true; 
 				foreach ($objekt->autori as $autor) {
 					if (!$isFirst) {
-						echo ", ";	
+						printf(", ");	
 					}
 					
-					echo '<a href="/katalog/autor/'.$autor->id.'/">'.trim($autor->titul_pred." ".$autor->jmeno." ".$autor->prijmeni." ".$autor->titul_za)."</a>";
+					printf('<a href="/katalog/autor/'.$autor->id.'/">'.trim($autor->titul_pred." ".$autor->jmeno." ".$autor->prijmeni." ".$autor->titul_za)."</a>");
 					
 					$isFirst = false;
 				}
@@ -108,7 +111,7 @@
 </div>
 
 <?php
-		if ($objCount % 3 == 0) echo '</div>';
+		if ($objCount % 3 == 0) printf('</div>');
 
 	}
 ?>	
@@ -131,44 +134,51 @@
 	if (count($objekty) > 0) {
 		$countPages = kv_object_pages_count();
 		
+		
+		$subcat = "";
+		$category = $oc->getCurrentCategory();
+		if ($category != null) {
+			$subcat = "kategorie/".$category->id."/";	
+		}
+		
 		if ($countPages > 0) {
 			// První
 			if ($page > 0) {
-				echo '<a href="/katalog/" class="strankovani-prvni">První</a>';		
+				printf('<a href="/katalog/'.$subcat.'" class="strankovani-prvni">První</a>');		
 			}		
 		
 			// Předchozí
 			if ($page-1 >= 0) {
-				echo '<a href="/katalog/?stranka='.($page-1).'" class="strankovani-predchozi">Předchozí</a>';		
+				printf('<a href="/katalog/'.$subcat.'?stranka='.($page-1).'" class="strankovani-predchozi">Předchozí</a>');		
 			}
 		
 			// před aktuální stránkou
 			if ($page-2 >= 0) {
-				echo '<a href="/katalog/?stranka='.($page-2).'" class="strankovani-polozka">'.($page-1)."</a>";		
+				printf('<a href="/katalog/'.$subcat.'?stranka='.($page-2).'" class="strankovani-polozka">'.($page-1)."</a>");		
 			}
 			if ($page-1 >= 0) {
-				echo '<a href="/katalog/?stranka='.($page-1).'" class="strankovani-polozka">'.($page)."</a>";		
+				printf('<a href="/katalog/'.$subcat.'?stranka='.($page-1).'" class="strankovani-polozka">'.($page)."</a>");		
 			}
 			
 			// aktuální
-			echo '<span class="strankovani-polozka-akt">'.($page+1).'</span>';
+			printf('<span class="strankovani-polozka-akt">'.($page+1).'</span>');
 			
 			// po aktuálním
 			if ($page <= $countPages) {
-				echo '<a href="/katalog/?stranka='.($page+1).'" class="strankovani-polozka">'.($page+2)."</a>";		
+				printf('<a href="/katalog/'.$subcat.'?stranka='.($page+1).'" class="strankovani-polozka">'.($page+2)."</a>");		
 			}
 			if ($page+1 <= $countPages) {
-				echo '<a href="/katalog/?stranka='.($page+2).'" class="strankovani-polozka">'.($page+3)."</a>";		
+				printf('<a href="/katalog/'.$subcat.'?stranka='.($page+2).'" class="strankovani-polozka">'.($page+3)."</a>");		
 			}
 			
 			// další
 			if ($page <= $countPages) {
-				echo '<a href="/katalog/?stranka='.($page+1).'" class="strankovani-dalsi">Další</a>';		
+				printf('<a href="/katalog/'.$subcat.'?stranka='.($page+1).'" class="strankovani-dalsi">Další</a>');		
 			}	
 			
 			// Poslední
 			if ($page <= $countPages) {
-				echo '<a href="/katalog/?stranka='.($countPages-1).'" class="strankovani-posledni">Poslední</a>';		
+				printf('<a href="/katalog/'.$subcat.'?stranka='.($countPages-1).'" class="strankovani-posledni">Poslední</a>');		
 			}
 		}	
 	}
