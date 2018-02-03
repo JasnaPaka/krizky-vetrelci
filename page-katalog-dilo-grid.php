037b84b630ab8af1f3ef8e09a17b7d7f1b5a81ab
Pavel Cvrček <jasnapaka@jasnapaka.com>
2018-02-03 17:50:01 +0100

    Autor: Nejsou známi #115

--- page-katalog-dilo-grid.php
+++ page-katalog-dilo-grid.php
@@ -2,7 +2,7 @@
         <a href="/katalog/kategorie/<?php printf($objekt->kategorie) ?>/" title="<?php print ($KV["zobrazeni_informaci"]) ?>"><?php printf($objekt->katnazev) ?></a></p>
 <p><span class="post-label"><?php if (count($objekt->autori) > 1) { printf("Autoři"); } else { printf("Autor"); } ?>:</span>
         <?php if (count($objekt->autori) == 0) { ?>
-                nejsou známi
+        <?php printf('<a href="/katalog/kategorie/'.$objekt->kategorie.'/bez-autora/">není znám</a>'); ?>
         <?php } else {
                 $isFirst = true; 
                 foreach ($objekt->autori as $autor) {
--- page-katalog-dilo-list.php
+++ page-katalog-dilo-list.php
@@ -1,6 +1,6 @@
 <h4><?php if (count($objekt->autori) > 1) { printf("Autoři"); } else { printf("Autor"); } ?>:</h4>
 <p><?php if (count($objekt->autori) == 0) { ?>
-    nejsou známi
+		<?php printf('<a href="/katalog/kategorie/'.$objekt->kategorie.'/bez-autora/">není znám</a>'); ?>
 <?php } else {
     $isFirst = true; 
     foreach ($objekt->autori as $autor) {
